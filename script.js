042f55c1a0e35027c40bf5221021ef8fc410f00c
HoangQuanGITHUB <mcinotgoodatgame@gmail.com>
2024-02-10 17:27:31 +0700

    website

---
+++ script.js
@@ -0,0 +1,15 @@
+function openmod1(){
+    open("https://steamcommunity.com/sharedfiles/filedetails/?id=3136902600");
+}
+function openmod2(){
+    open("https://steamcommunity.com/sharedfiles/filedetails/?id=3140157826");
+}
+function openmod3(){
+    open("https://steamcommunity.com/sharedfiles/filedetails/?id=3152003900");
+}
+function openmod4(){
+    open("https://steamcommunity.com/sharedfiles/filedetails/?id=3138450738");
+}
+function openmod5(){
+    open("https://steamcommunity.com/sharedfiles/filedetails/?id=3148042392");
+}
